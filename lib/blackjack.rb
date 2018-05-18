68740843002b2d536ba33ecd9e89a153aee4b8b6
Malcolm Leake <echelon1089@gmail.com>
2018-05-18 20:56:44 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -66,12 +66,9 @@ def runner
   welcome
   card_total = initial_round
   display_card_total(card_total)
- end_game(card_total)
  until card_total > 21
-  end_game(card_total
-
   card_total = hit?(card_total)
 end
-
+ end_game(card_total)
   end
     
